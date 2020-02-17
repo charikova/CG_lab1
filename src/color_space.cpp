cddffd82ab792899358fc2eef19d92422838d83b
Beth Johanssen <m.charikova@innopolis.ru>
2020-02-17 21:27:09 +0300

    Color space

--- src/color_space.cpp
+++ src/color_space.cpp
@@ -10,14 +10,18 @@ ColorSpace::~ColorSpace()
 {
 }
 
-void ColorSpace::DrawScene()
-{
-
-
+void ColorSpace::DrawScene() {
+    for (unsigned short x = 0; x < width; x++) {
+        for (unsigned short y = 0; y < height; y++) {
+            SetPixel(x, y, color(
+                    static_cast<unsigned char>(255)* x / static_cast<float>(width),
+                    static_cast<unsigned char>(255)* y / static_cast<float>(height),
+                    0));
+        }
+    }
 }
 
-void ColorSpace::SetPixel(unsigned short x, unsigned short y, color color)
-{
-
+void ColorSpace::SetPixel(unsigned short x, unsigned short y, color color){
+    frame_buffer[y * width + x] = color;
 }
 
